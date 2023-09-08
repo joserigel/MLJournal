642b12c0881cb74e070185dd2b31b2106e76e8c3
joserigel <joserigel18@gmail.com>
2023-09-08 14:58:12 +0200

    Added dataset exercise

---
+++ personal_optim.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from tqdm import tqdm
+
+# Get Device
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Targets
+data = {
+    "DOG" : torch.tensor([1, 0, 0, 0], dtype=torch.uint8, device=device),
+    "CAT" : torch.tensor([0, 1, 0, 0], dtype=torch.uint8, device=device),
+    "BIRD":  torch.tensor([0, 0, 1, 0], dtype=torch.uint8, device=device),
+    "FISH":  torch.tensor([0, 0, 0, 1], dtype=torch.uint8, device=device),
+}
+targets = {
+    "DOG": 1,
+    "CAT": 2/3,
+    "BIRD": 1/3,
+    "FISH": 0
+}
+
+# Neural Network
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.fc = nn.Linear(4, 1)
+    def forward(self, x):
+        x = x / x.sum()
+        x = self.fc(x)
+        return x
+
+# Init model
+model = NeuralNetwork()
+model.fc.weight.data = torch.ones_like(model.fc.weight.data)
+model.fc.bias.data = torch.zeros_like(model.fc.bias.data)
+model.zero_grad()
+if (torch.cuda.is_available()):
+    model = model.cuda()
+
+print("="*5, "BEFORE", "="*5)
+for name, param in model.named_parameters():
+    print(f'{name}: {param.data}')    
+
+print("="*5, "TRAIN", "="*5)
+# Optimizing Model
+learning_rate = 0.01
+iteration = 1000
+optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+for i in tqdm(range(iteration)):
+    for key, value in data.items():
+        # model.zero_grad()
+        optimizer.zero_grad()
+        output = model(value)
+        target = torch.full_like(output, targets[key], device=device)
+        criterion = nn.MSELoss()
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+
+        
+
+print("="*5, "AFTER", "="*5)
+for name, param in model.named_parameters():
+    print(f'{name}: {param.data}')    
+
+print("="*5, "CHECK", "="*5)
+for key, value in data.items():
+    output = model(value)
+    print(f'{key}: {round(output.cpu().detach().numpy()[0], 2)}, target: {targets[key]}')
